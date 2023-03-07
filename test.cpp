37d2c7fe69215a187503bdee33979ebb6fa54afc
qz1027 <3315057006@qq.com>
2023-03-07 18:59:21 +0800

    test01

--- test.cpp
+++ test.cpp
@@ -1,8 +1,25 @@
 #include<iostream>
 using namespace std;
 
+double test01(double x){
+	return x * x;
+}
+
+double test02(double& x){
+	return x * x;
+}
+
+void test03(){
+	double a = 0;
+	cout << "Please input a num:";
+	cin >> a;
+	cout << "The num square is:" << test01(a) << endl; 
+}
+
+
+
 int main(){
-	cout << "Hello world!" << endl;
+	test03();
 
 	return 0;
 }
